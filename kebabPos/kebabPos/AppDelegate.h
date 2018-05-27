b0b32c98e11b322c770fd5a57073cace357ef39f
Amir's Macbook Pro <>
2018-05-27 23:14:22 +1000

    Init Project

---
+++ kebabPos/kebabPos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kebabPos
+//
+//  Created by Amir Kamali on 27/5/18.
+//  Copyright © 2018 Assembly Payments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kebabPos/kebabPos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kebabPos
+//
+//  Created by Amir Kamali on 27/5/18.
+//  Copyright © 2018 Assembly Payments. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
