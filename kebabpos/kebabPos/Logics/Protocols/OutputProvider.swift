bef0a2d6d916457bcccb710268784342723772af
Amir's Macbook Pro <>
2018-06-11 02:23:36 +1000

    group2

---
+++ kebabpos/kebabPos/Logics/Protocols/OutputProvider.swift
@@ -0,0 +1,12 @@
+//
+//  OutputProvider.swift
+//  kebabPos
+//
+//  Created by Amir Kamali on 10/6/18.
+//  Copyright © 2018 Assembly Payments. All rights reserved.
+//
+
+import Foundation
+protocol OutputProvider{
+    func logMessage(_ message:String)
+}
